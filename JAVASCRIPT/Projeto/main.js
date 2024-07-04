f756fedae5e5e9d50995681455eee27979882de9
Roger <jesusrogercosta2004@gmail.com>
2024-07-03 21:38:48 -0300

    Segundo commit

--- JAVASCRIPT/Projeto/main.js
+++ JAVASCRIPT/Projeto/main.js
@@ -1,1 +1,6 @@
-alert ('Olá, mundo ');
+function tocaSomPom (){     //Declaracao de função
+   document.querySelector('#som_tecla_pom').play();
+}          
+document.querySelector('.tecla_pom'). onclick = tocaSomPom; //Busca o conteúdo no html e executa a função 
+
+
