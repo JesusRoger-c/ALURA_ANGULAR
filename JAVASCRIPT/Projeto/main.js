d6aec45d62971f6ac1e832ed94028fae2c9424b1
Roger <jesusrogercosta2004@gmail.com>
2024-07-04 16:28:48 -0300

    Terceiro commit

--- JAVASCRIPT/Projeto/main.js
+++ JAVASCRIPT/Projeto/main.js
@@ -1,6 +1,24 @@
-function tocaSomPom (){     //Declaracao de função
-   document.querySelector('#som_tecla_pom').play();
-}          
-document.querySelector('.tecla_pom'). onclick = tocaSomPom; //Busca o conteúdo no html e executa a função 
+function tocaSom (idElementoAudio){     //Declaracao de função
+   document.querySelector(idElementoAudio).play();
+} 
 
 
+const listaDeTeclas = document.querySelectorAll('.tecla');  //Criando uma lista constante
+let contador = 0;                                          // Criando uma variável que entra informação direto (referência variável)
+
+
+//enquanto - laço de repetição 
+while(contador < listaDeTeclas.length){
+
+   const tecla = listaDeTeclas[contador]
+   const instrumento = tecla.classList[1];
+   const idAudio = `#som_${instrumento}`;      // ${} - indica que há uma variável ali dentro 
+
+   console.log(idAudio);
+   tecla.onclick = function () {
+   tocaSom(idAudio);
+   };
+
+   contador = contador + 1;                               // Sair do looping, incrementar +1  
+   console.log(contador);                                //imprime no console a informação digitada 
+}
