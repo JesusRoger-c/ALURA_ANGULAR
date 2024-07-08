61e9bf37275dd5395fe5edabb4ffbcf53c7da0bb
Roger <jesusrogercosta2004@gmail.com>
2024-07-08 20:44:46 -0300

    Atualizações no javaScript

--- JAVASCRIPT/Projeto/main.js
+++ JAVASCRIPT/Projeto/main.js
@@ -4,21 +4,27 @@ function tocaSom (idElementoAudio){     //Declaracao de função
 
 
 const listaDeTeclas = document.querySelectorAll('.tecla');  //Criando uma lista constante
-let contador = 0;                                          // Criando uma variável que entra informação direto (referência variável)
 
 
-//enquanto - laço de repetição 
-while(contador < listaDeTeclas.length){
+//para - laço de repetição 
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
 
    const tecla = listaDeTeclas[contador]
    const instrumento = tecla.classList[1];
    const idAudio = `#som_${instrumento}`;      // ${} - indica que há uma variável ali dentro 
-
-   console.log(idAudio);
+   
    tecla.onclick = function () {
    tocaSom(idAudio);
-   };
+   }
 
-   contador = contador + 1;                               // Sair do looping, incrementar +1  
-   console.log(contador);                                //imprime no console a informação digitada 
+   tecla.onkeydown = function (evento) {    //Pintar o botão ao clicar 
+      if(evento.code === 'Space' || evento.code === 'Enter'){
+      tecla.classList.add('ativa');
+      }
+   }
+
+   tecla.onkeyup = function (){         // Remover a cor ao parar de apertar 
+      tecla.classList.remove('ativa');
+   }
+
 }
