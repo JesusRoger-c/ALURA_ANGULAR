b6e4bdcf09d785c448a73ebd57fe8f505b87ba45
Roger <jesusrogercosta2004@gmail.com>
2024-10-14 10:52:04 -0300

    Primeiro commit

--- TYPESCRIPT/formacao-typescript-projeto-curso01-projeto-base/formacao-typescript-projeto-curso01-projeto-base/js/script.js
+++ TYPESCRIPT/formacao-typescript-projeto-curso01-projeto-base/formacao-typescript-projeto-curso01-projeto-base/js/script.js
@@ -1,4 +1,4 @@
-let saldo = 3000;
+let saldo = 3000.00;
 
 
 //Selecione o saldo-valor e Procure o elemento valor  
@@ -14,4 +14,38 @@ elementoFormulario = addEventListener("submit", function(event) {
     alert("Por favor, preencha todos os dados da transação!");
     return;
  }
+    // Buscar valor, data e tipo de transacao 
+    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+    const inputValor = elementoFormulario.querySelector("#valor");
+    const inputData = elementoFormulario.querySelector("#data");
+
+
+    let tipoTransacao = inputTipoTransacao.value; // Encontrar o valor registrado no formulário (Tipo transacao) 
+    let valor = inputValor.value;
+    let data = inputData.value;
+
+   
+    //Validação de valores 
+    if(tipoTransacao == "Depósito"){
+      saldo += valor;
+    }else if(tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto"){
+      saldo -= valor;
+    }else{
+      alert("Transacao inválida");
+      return;
+    }
+
+   //Estou atualizando o painel de valores de acordo com a transacao 
+   elementoSaldo.textContent = saldo;
+
+    //Criação de um objeto com a finalidade de armazenar as informações obtidas
+
+    const novaTransacao = {
+    tipoTransacao: tipoTransacao,
+    valor: valor,
+    data: data
+    }
+
+    console.log(novaTransacao);
+    elementoFormulario.reset(); // Uma vez que eu puxei os dados, deve ser resetado o formulario 
 });
